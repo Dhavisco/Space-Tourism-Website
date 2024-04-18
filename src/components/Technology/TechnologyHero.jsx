76b4b5163bfdd49b0d922e24aeb59816674da606
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-18 21:52:40 +0100

    changed interval

--- src/components/Technology/TechnologyHero.jsx
+++ src/components/Technology/TechnologyHero.jsx
@@ -11,7 +11,7 @@ const TechnologyHero = () => {
  const intervalId = setInterval(()=>{
   const nextIndex = (currentTechIndex + 1) % tech.length;
     setcurrentTechIndex(nextIndex);
- },5000)
+ },15000)
 
  return () => clearInterval(intervalId);
 
