30ecf7085b22510308c82cf52ba85d280d6b7b2b
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-18 22:23:09 +0100

    added react icon

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -8,15 +8,10 @@ function Loading() {
       className="flex items-center place-content-center bg-black h-screen w-screen relative"
     >
       <div className="flex-1 text-center pt-2 justify-center items-center flex relative flex-col">
-        <img
-          src={img}
-          alt="loader"
-          loading="lazy"
-          className="w-[30%] lg:w-[14rem] lg:h-[16rem] ml-2"
-        />{" "}
+        <img src={img} alt="loader" loading="lazy" className="w-[20%] h-auto pb-2" />{" "}
         <RiLoader2Fill className="text-white mt-2 animate-spin" />
         <p
-          className="lg:text-4xl mt-1 text-white text-2xl tracking-wide animate-pulse"
+          className="lg:text-4xl mt-1 text-white text-2xl  text-center tracking-wide animate-pulse"
           style={{ fontFamily: "Dancing Script" }}
         >
           LOADING...
--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -1,20 +1,21 @@
 // import Header from "./Header";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Loading from "../../Loading";
 import Hero from "./Hero";
 
 
 const Index = () => {
-  //  const [loading, setLoading] = useState(true);
+   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setLoading(false), 1000);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1000);
 
-  //   return () => clearTimeout(timer);
-  // }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // if (loading) {
-  //   return <Loading />;
-  // }
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
 
