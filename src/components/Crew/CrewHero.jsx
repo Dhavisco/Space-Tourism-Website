256151f8c0964c2f89d8086c334a870b1fa433e0
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-18 21:53:50 +0100

    updated interval-crew

--- src/components/Crew/CrewHero.jsx
+++ src/components/Crew/CrewHero.jsx
@@ -14,7 +14,7 @@ useEffect(()=>{
   const intervalId = setInterval(() => {
     const nextIndex = (currentCrewIndex + 1) % crew.length;
     setCurrentCrewIndex(nextIndex);
-  }, 3000);
+  }, 15000);
 
   return () => clearInterval(intervalId); //clear interval on component unmount
 }, [currentCrewIndex, crew.length]);
