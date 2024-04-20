79d526c46dff70bdff404b7945de9efd01aa0697
Dhavisco <olaniyidavid2002@gmail.com>
2024-04-20 20:27:30 +0100

    added border to explore

--- src/components/Home/HeroText.jsx
+++ src/components/Home/HeroText.jsx
@@ -21,7 +21,7 @@ const HeroText = () => {
         
           <div className="flex flex-col justify-end items-end lg:mt-[14.2rem]">
             <Link to="/destination">
-            <div className="flex justify-center items-center tracking-widest leading-relaxed text-xl lg:text-2xl font-[Sans-serif] font-[50] bg-white text-[#111142c9] rounded-full w-[10rem] h-[10rem] lg:w-[13rem] lg:h-[13rem] uppercase cursor-pointer hover:bg-[#b1b1ee] hover:text-[#111142f1] transition-colors duration-300">
+            <div className="flex justify-center items-center tracking-widest leading-relaxed text-xl lg:text-2xl font-[Sans-serif] font-[50] border-b-2 border-blue-900 bg-white text-[#111142c9] rounded-full w-[10rem] h-[10rem] lg:w-[13rem] lg:h-[13rem] uppercase cursor-pointer hover:bg-[#b1b1ee] hover:text-[#111142f1] transition-colors duration-300">
               Explore
             </div>
             </Link>
